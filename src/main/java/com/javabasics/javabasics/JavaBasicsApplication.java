0f8bc2315b41578122254eb9eeb6c7ec9106aede
Venkat_Seshasai <seshasai.venkat@gmail.com>
2022-08-19 09:25:51 +0530

    Push it to Git

--- src/main/java/com/javabasics/javabasics/JavaBasicsApplication.java
+++ src/main/java/com/javabasics/javabasics/JavaBasicsApplication.java
@@ -9,5 +9,5 @@ public class JavaBasicsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaBasicsApplication.class, args);
 	}
-
+		//My Changes
 }
---
+++ src/main/java/javaobjects/MyFirstJava.kt
@@ -0,0 +1,5 @@
+package javaobjects
+
+class MyFirstJava {
+    //Changes
+}
